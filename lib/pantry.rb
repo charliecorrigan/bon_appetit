749ae8e0bb9b9f6eeb8f4757496e63bc5028650e
ccorrigan <barnumswinter@gmail.com>
2017-04-18 09:51:49 -0600

    Finish #stock_check and #restock functionality in pantry.rb

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -8,6 +8,17 @@ class Pantry
   def stock_check(item)
     if stock[item].nil?
       return 0
+    else
+      return stock[item]
+    end
+  end
+
+  def restock(item, amount)
+    if stock[item].nil?
+      @stock[item] = amount
+    else
+      current_amount = stock[item]
+      @stock[item] = amount + current_amount
     end
   end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -16,10 +16,47 @@ class PantryTest < Minitest::Test
     assert output.empty?
   end
 
-  def test_stock_check_returns_accurate_inventory
+  def test_stock_check_returns_zero_when_empty
     pantry = Pantry.new
     output = pantry.stock_check("Cheese")
     assert_equal 0, output
   end
 
+  def test_restock_adds_item_and_quantity_to_stock
+    pantry = Pantry.new
+
+    output_1 = pantry.stock
+    assert output_1.empty?
+    
+    pantry.restock("Cheese", 10)
+    
+    output_2 = pantry.stock
+    refute output_2.empty?
+  end
+
+  def test_stock_check_returns_stock_amount_when_item_is_in_stock
+    pantry = Pantry.new
+    
+    output_1 = pantry.stock_check("Cheese")
+    assert_equal 0, output_1
+    
+    pantry.restock("Cheese", 10)
+    output_2 = pantry.stock_check("Cheese")
+    assert_equal 10, output_2
+  end
+
+  def test_restock_adds_amount_to_item_instead_of_replacing_amount
+    pantry = Pantry.new
+    
+    output_1 = pantry.stock_check("Cheese")
+    assert_equal 0, output_1
+    
+    pantry.restock("Cheese", 10)
+    output_2 = pantry.stock_check("Cheese")
+    assert_equal 10, output_2
+
+    pantry.restock("Cheese", 20)
+    output_3 = pantry.stock_check("Cheese")
+    assert_equal 30, output_3
+  end
 end
