a308ad9e3e9424ec3362d6d850f43b7c727f39c2
ccorrigan <barnumswinter@gmail.com>
2017-04-18 09:40:12 -0600

    Add #initialize and start #stock_check functionality

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,13 @@
 class Pantry
+  attr_reader :stock
 
+  def initialize
+    @stock = {}
+  end
+
+  def stock_check(item)
+    if stock[item].nil?
+      return 0
+    end
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,22 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  def test_it_exists
+    pantry = Pantry.new
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_pantry_stock_attribute
+    pantry = Pantry.new
+    output = pantry.stock
+    assert_equal Hash, output.class
+    assert output.empty?
+  end
+
+  def test_stock_check_returns_accurate_inventory
+    pantry = Pantry.new
+    output = pantry.stock_check("Cheese")
+    assert_equal 0, output
+  end
+
 end
